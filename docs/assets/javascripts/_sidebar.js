239c991e3b0c03a2a05c31c0bdda60e613642b74
Fernando Moreira <nandomoreira.me@gmail.com>
2017-03-12 22:46:34 -0300

    Sidebar accordion style

--- docs/assets/javascripts/_sidebar.js
+++ docs/assets/javascripts/_sidebar.js
@@ -1,7 +1,6 @@
 const sidebar = (function() {
   const accordions = document.getElementsByClassName('js-accordion');
 
-  // Add click listeners to all the accordion triggers
   for (i = 0; i < accordions.length; i++) {
     accordionTrigger = accordions[i].getElementsByClassName('js-accordion-trigger');
 
@@ -12,7 +11,7 @@ const sidebar = (function() {
 
   function toggleAccordion(e) {
     e.preventDefault();
-    accordionContent = this.nextElementSibling;
-    accordionContent.classList.toggle('is-hidden');
+    accordionContent = this.parentNode;
+    accordionContent.classList.toggle('is-active');
   }
 })();
